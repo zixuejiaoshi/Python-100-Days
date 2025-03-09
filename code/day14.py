69d3a4268da612623d0a54ae9806d28a242733a1
peng <23767990@qq.com>
2025-03-09 10:20:00 +0800

    新增代码文件

---
+++ code/day14.py
@@ -0,0 +1,15 @@
+# 函数
+
+# 输入m和n，计算组合数C(m,n)的值
+def fac(num):
+    result = 1
+    for n in range(2, num + 1):
+        result *= n
+    return result
+
+
+m = int(input('m = '))
+n = int(input('n = '))
+# 计算阶乘的时候不需要写重复的代码而是直接调用函数
+# 调用函数的语法是在函数名后面跟上圆括号并传入参数
+print(fac(m) // fac(n) // fac(m - n))
